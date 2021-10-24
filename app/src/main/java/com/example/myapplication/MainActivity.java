9656450bd501e36cb1ae6532ff66cb2ab5b8ab06
usama <bitf18a534@pucit.edu.pk>
2021-10-24 16:54:16 +0500

    textView is added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create references
+
+        EditText firstText =(EditText) findViewById(R.id.text1);
+        TextView result =(TextView) findViewById(R.id.textView);
+        Button onMoney = (Button) findViewById(R.id.button1);
+
+        //onclick button
 
 
 
